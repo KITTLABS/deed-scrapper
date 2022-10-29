b25be10adb27f4d60f3a74f012e09c05fa3ef174
jlyoungthe3rd <jlyoungthe3rd@gmail.com>
2022-10-29 16:39:42 -0700

    initial setup for packages and type definitions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,23 @@
-console.log('Hello world!');
+import puppeteer from 'puppeteer'
 
+const dataSourceInitialPage = 'https://www.gsccca.org/';
+const username = 'dmSmith';
+const password = '799Ln38Lsjqs!yg^99wfs*9ahYo6L8';
+
+async function navigateToSource(address: string) {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+
+  await page.goto(dataSourceInitialPage);
+
+  browser.close();
+}
+
+//returns data to scrape
+async function scrapeData() {
+  let data = {};
+  return data;
+}
+
+navigateToSource(dataSourceInitialPage);
+// scrapeData()
