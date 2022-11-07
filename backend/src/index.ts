406c254a2d2a82f3a1b1520ead36dcd1231d33c4
jlyoungthe3rd <jlyoungthe3rd@gmail.com>
2022-11-06 19:35:00 -0800

    created function to navigate to data source

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,66 @@
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
 
-const dataSourceInitialPage = 'https://www.gsccca.org/';
-const username = 'dmSmith';
-const password = '799Ln38Lsjqs!yg^99wfs*9ahYo6L8';
+const _DATASOURCEPAGELOGIN = 'https://www.gsccca.org/';
+const _DATASOURCEPAGESEARCH =
+  'https://search.gsccca.org/PT61Premium/AddressSearch.aspx';
+const _USERNAME = 'dmSmith';
+const _PASSWORD = '799Ln38Lsjqs!yg^99wfs*9ahYo6L8';
+//hard-coded for now
+const _ADDRESS = '242 Mayson Ave NE';
+
+// interface DeedData {
+//   count: string;
+//   pt61Number: string;
+//   dateOfSale: Date;
+// }
 
 async function navigateToSource(address: string) {
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch({ headless: false, devtools: true });
   const page = await browser.newPage();
+  const navPromise = page.waitForNavigation();
 
-  await page.goto(dataSourceInitialPage);
+  //goto deed data site
+  await page.goto(_DATASOURCEPAGELOGIN);
 
-  browser.close();
+  //click login [top right]
+  await page.click('#head > div > ul > li.pnlLogin');
+
+  //input username in login popup
+  await page.type('#username', _USERNAME);
+
+  //input password in login popup
+  await page.type('#password', _PASSWORD);
+
+  //click login button in login popup
+  //then wait for login to complete
+  await page.click('#loginbtn');
+  await page.waitForSelector('#head > div > ul > li.pnlLogout.show-element', {
+    visible: true,
+  });
+
+  //goto search
+  await page.goto(_DATASOURCEPAGESEARCH);
+
+  //input address
+  await page.type('#BodyContent_txtAddress', address);
+
+  //click 'begin search'
+  await Promise.all([
+    page.click('#BodyContent_btnSearch'),
+    page.waitForNavigation(),
+  ]);
+
+  await page.waitForSelector('#BodyContent_lvDashboard_btnViewPT61_0');
+  //click 'view pt-61 information'
+  await page.click('#BodyContent_lvDashboard_btnViewPT61_0');
+
+  // browser.close();
 }
 
-//returns data to scrape
-async function scrapeData() {
-  let data = {};
-  return data;
-}
+//TODO: returns  organized scraped data as an object
+// async function scrapeData() {
+//   let data = {};
+//   return data;
+// }
 
-navigateToSource(dataSourceInitialPage);
-// scrapeData()
+navigateToSource(_ADDRESS);
