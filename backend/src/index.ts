5ab6911f98b38e40b0ffb1f6b496972d4f79c2f1
jlyoungthe3rd <jlyoungthe3rd@gmail.com>
2022-11-12 15:13:27 -0800

    created function to scrape data and return as object

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,13 +8,23 @@ const _PASSWORD = '799Ln38Lsjqs!yg^99wfs*9ahYo6L8';
 //hard-coded for now
 const _ADDRESS = '242 Mayson Ave NE';
 
-// interface DeedData {
-//   count: string;
-//   pt61Number: string;
-//   dateOfSale: Date;
-// }
+interface DeedData {
+  county: string;
+  dateOfSale: Date;
+  deedBook: number;
+  deedPage: number;
+  propertyAddress: string;
+  propertyValue: number;
+  propertyTaxDue: number;
+  propertyLandDistrict: string;
+  propertyLandLot: number;
+  buyerName: string;
+  buyerAddress: string;
+  sellerName: string;
+  sellerAddress: string;
+}
 
-async function navigateToSource(address: string) {
+async function navigateToSource(address: string): Promise<void> {
   const browser = await puppeteer.launch({ headless: false, devtools: true });
   const page = await browser.newPage();
   const navPromise = page.waitForNavigation();
@@ -54,13 +64,77 @@ async function navigateToSource(address: string) {
   //click 'view pt-61 information'
   await page.click('#BodyContent_lvDashboard_btnViewPT61_0');
 
+  scrapeData(page);
+
   // browser.close();
 }
 
-//TODO: returns  organized scraped data as an object
-// async function scrapeData() {
-//   let data = {};
-//   return data;
-// }
+async function scrapeData(page: Page): Promise<DeedData> {
+  const county = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblCountyName_0`
+  );
+
+  const dateOfSale = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblDateOfSale_0`
+  );
+
+  const deedBook = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblDeedBook_0`
+  );
+  const deedPage = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblDeedPage_0`
+  );
+  const propertyValue = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblActualValue_0`
+  );
+  const propertyTaxDue = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblTaxDue_0`
+  );
+  const propertyLandDistrict = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblLandDistrict_0`
+  );
+  const propertyLandLot = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblLandLot_0`
+  );
+  const buyerName = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblBuyerName_0`
+  );
+  const buyerAddress = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblBuyerAddress_0`
+  );
+  const sellerName = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblSellerName_0`
+  );
+  const sellerAddress = await page.waitForSelector(
+    `#BodyContent_lvFinalViews_ucCombinedQuickView_0_ucPT61QuickView_0_lblSellerAddress_0`
+  );
+
+  const DeedData = {
+    county: await county.evaluate((e) => e.textContent),
+    dateOfSale: new Date(await dateOfSale.evaluate((e) => e.textContent)),
+    deedBook: parseInt(await deedBook.evaluate((e) => e.textContent)),
+    deedPage: parseInt(await deedPage.evaluate((e) => e.textContent)),
+    //!! hard coded for now
+    propertyAddress: _ADDRESS,
+    propertyValue: parseInt(
+      await propertyValue.evaluate((e) => e.textContent.replace(/[^0-9]/g, ''))
+    ),
+    propertyTaxDue: parseInt(
+      await propertyTaxDue.evaluate((e) => e.textContent.replace(/[^0-9]/g, ''))
+    ),
+    propertyLandDistrict: await propertyLandDistrict.evaluate(
+      (e) => e.textContent
+    ),
+    propertyLandLot: parseInt(
+      await propertyLandLot.evaluate((e) => e.textContent)
+    ),
+    buyerName: await buyerName.evaluate((e) => e.textContent),
+    buyerAddress: await buyerAddress.evaluate((e) => e.textContent),
+    sellerName: await sellerName.evaluate((e) => e.textContent),
+    sellerAddress: await sellerAddress.evaluate((e) => e.textContent),
+  };
+  console.log(DeedData);
+  return DeedData;
+}
 
 navigateToSource(_ADDRESS);
